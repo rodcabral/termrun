036492dd3224309ffd31078609fead9342b70391
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2024-03-25 01:45:26 -0300

    new example

--- example/main.c
+++ example/main.c
@@ -1,59 +1,77 @@
-#include <termrun.h>
+#include "trm_window.h"
 #include <stdbool.h>
+#include <termrun.h>
+#include <unistd.h>
+
+#define WINDOW_WIDTH 80
+#define WINDOW_HEIGHT 30
 
 int main() {
     TRM_Context* context = TRM_CreateContext();
-    TRM_Window* window = TRM_CreateWindow(40, 20);
+    TRM_Window* window = TRM_CreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    context->objects[0].x = 5;
-    context->objects[0].y = 5;
-    context->objects[0].ch = '@';
+    int player_x = 1;
+    int player_y = 1;
 
-    context->objects[2].x = 16;
-    context->objects[2].y = 9;
-    context->objects[2].ch = 'X';
-    
-    context->objects[3].x = 10;
-    context->objects[3].y = 10;
-    context->objects[3].ch = 'V';
+    context->objects[0].x = player_x;
+    context->objects[0].y = player_y;
+    context->objects[0].ch = '#';
 
-    context->objects[1].x = 6;
-    context->objects[1].y = 17;
-    context->objects[1].ch = '&';
-
-    TRM_Event event;
+    context->objects[1].x = 10;
+    context->objects[1].y = 10;
+    context->objects[1].ch = '@';
 
     bool loop = true;
+    TRM_Event event;
     while(loop) {
         TRM_UpdateWindow(window, context);
-
         TRM_GetKey(&event);
 
+        if(event.key == 'q') {
+            loop = false;
+        }
+
+        context->objects[0].x += player_x;
+
+        if(context->objects[0].x >= WINDOW_WIDTH - 1) {
+            player_x--;
+        }
+
+        if(context->objects[0].x <= 0) {
+            player_x++;
+        }
+
+        context->objects[0].y += player_y;
+
+        if(context->objects[0].y >= WINDOW_HEIGHT - 1) {
+            player_y--;
+        }
+
+        if(context->objects[0].y <= 0) {
+            player_y++;
+        }
+
         switch(event.key) {
-            case 'q':
-                TRM_CloseWindow();
-                loop = false;
-                break;
             case TRM_UP:
-                context->objects[3].y--;
+                context->objects[1].y--;
                 break;
             case TRM_DOWN:
-                context->objects[3].y++;
+                context->objects[1].y++;
                 break;
             case TRM_RIGHT:
-                context->objects[3].x++;
+                context->objects[1].x++;
                 break;
             case TRM_LEFT:
-                context->objects[3].x--;
+                context->objects[1].x--;
                 break;
-        }   
+        }
 
         TRM_RenderWindow(window);
     }
 
-    TRM_CloseWindow();
     TRM_DestroyContext(context);
     TRM_DestroyWindow(window);
+    TRM_CloseWindow();
 
     return 0;
 }
